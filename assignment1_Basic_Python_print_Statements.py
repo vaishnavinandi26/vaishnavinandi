44225cb08c4766bb2316ee635123ce2b1c55fd39
vaishnavinandi <vaishnavinandi26@gmail.com>
2025-11-07 11:54:46 +0530

    assignment

---
+++ assignment1_Basic_Python_print_Statements.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+a
+
+
+# In[2]:
+
+
+print(5,10,15)
+
+
+# In[6]:
+
+
+name="Vaishnavi"
+print(name)
+
+
+# In[7]:
+
+
+firstname="Vaishnavi"
+lastname="Nandi"
+print(firstname,lastname)
+
+
+# In[11]:
+
+
+age=25
+print("I am",age,"years old")
+
+
+# In[15]:
+
+
+name="Vaishnavi"
+city="Karimnagar"
+print("My name is {} and I live in {}".format(name,city))
+
+
+# In[20]:
+
+
+a=10
+b=20
+c=30
+print(a,"|",b,"|",c)
+
+
+# In[25]:
+
+
+a= 'Python and Rocks!'
+print(a)
+
+
+# In[28]:
+
+
+a=5
+b=8
+print(f' A {a} and B {b} sum is {a+B}')
+
+
+# In[31]:
+
+
+user_name=input("Enter your name")
+print("Hello ", user_name)
+
+
+# In[33]:
+
+
+city="Karimnagar"
+print(city)
+
+
+# In[35]:
+
+
+a=10
+b=20
+a,b=b,a
+print(a,b)
+
+
+# In[36]:
+
+
+(a,b,c)=(5,10,"Python")
+print(a,b,c)
+
+
+# In[38]:
+
+
+col1="Red"
+col2="Red"
+col3="Red"
+print(col1,col2,col3)
+
+
+# In[39]:
+
+
+num=3.14
+print(num,type(num))
+
+
+# In[42]:
+
+
+name="vaishnavi"
+age=25
+print("My name is {} and I am {} years old".format(name,age))
+
+
+# In[43]:
+
+
+a="5"
+b=3
+print(a*b)
+
+
+# In[44]:
+
+
+is_python_fun=True
+print(is_python_fun,type(is_python_fun))
+
+
+# In[45]:
+
+
+x=10
+x+=5
+print(x)
+
+
+# In[46]:
+
+
+a=b=c=5
+b=10
+print(a,b,c)
+
+
+# In[47]:
+
+
+a=b=c=d=e=0
+b=1
+print(a,b,c,d,e)
+
+
+# In[55]:
+
+
+x=10
+del(x) 
+print(x)
+
+
+# In[62]:
+
+
+x=5
+y=7
+print(x+y)
+
+
+# In[61]:
+
+
+a=10
+b=20
+a,b=b,a
+print(a,b)
+
+
+# In[63]:
+
+
+a="Hello"
+print(a)
+
+
+# In[64]:
+
+
+result=5*8
+print(result)
+
+
+# In[66]:
+
+
+a,b=10,20
+print(a,b)
+
+
+# In[67]:
+
+
+firstname="Vaishnavi"
+lastname="Nandi"
+print(firstname,lastname)
+
+
+# In[68]:
+
+
+greeting="Good Morning"
+del(greeting)
+print(greeting)
+
+
+# In[70]:
+
+
+num=10
+num2=num+5
+print(num2)
+
+
+# In[ ]:
+
+
+
+
---
+++ assignment2_Python_Datatypes_5-11-25.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=int(input("Enter any value:"))
+b=int(input("Enter any Value:"))
+print(a+b)
+
+
+# In[3]:
+
+
+a=float(input("Enter any value:"))
+
+
+# In[5]:
+
+
+age=int(input("Enter your age:"))
+c=100-age
+print("Years left to reach 100 is:",c)
+
+
+# In[6]:
+
+
+r=float(input("Enter radius of a circle:"))
+area_of_circle=3.14*r*r
+print(area_of_circle)
+
+
+# In[7]:
+
+
+fname=input("Enter your firstname:")
+lname=input("Enter your lastname:")
+print(fname,lname)
+
+
+# In[8]:
+
+
+a=int(input("enter any value:"))
+b=a*2
+print(b)
+
+
+# In[52]:
+
+
+a=input("enter any math expression")
+result=eval(a)
+print(result)
+
+
+# In[15]:
+
+
+a=input("Enter any value:")
+print(a,type(a))
+b=int(a)
+c=b+10
+print(c)
+
+
+# In[16]:
+
+
+a=float(input("Enter any value:"))
+print(a,type(a))
+b=int(a)
+print(b,type(b))
+
+
+# In[17]:
+
+
+user_name=input("Enter user name:")
+print("welcome",user_name)
+
+
+# In[18]:
+
+
+a=12
+b=5
+print(a+b)
+
+
+# In[19]:
+
+
+x=10
+y=3
+print()
+print((x*y)-x,y)
+
+
+# In[20]:
+
+
+x=True
+print(x,type(x))
+
+
+# In[23]:
+
+
+x=17
+y=4
+print(x%y)
+
+
+# In[24]:
+
+
+a=2
+b=3
+c=4
+print("result:",(2*3)-4)
+print(a-b-c)
+
+
+# In[26]:
+
+
+a=int(input("Form a number with 5,6,9:"))
+print(a,type(a))
+
+
+# In[27]:
+
+
+a=10
+b=25
+print(a-b)
+
+
+# In[30]:
+
+
+a=99
+print(a)
+print(a+1)
+
+
+# In[33]:
+
+
+a=4**2
+print(a)
+print(a+1)
+
+
+# In[34]:
+
+
+number=6*10
+print(number-5)
+
+
+# In[35]:
+
+
+a=1.234
+b=2.456
+print(a+b,type(a+b))
+
+
+# In[36]:
+
+
+r=2.5
+area_of_circle=3.14*r*r
+print(area_of_circle)
+
+
+# In[37]:
+
+
+a=2.5
+print(a,type(a))
+print(1000*a)
+
+
+# In[40]:
+
+
+a=2.4
+b=4+a
+print(b,type(b))
+
+
+# In[43]:
+
+
+a=2.5
+b=3.5
+c=7.0
+average=a+b+c/3
+print("Average of float numbers:",average)
+
+
+# In[44]:
+
+
+a=5
+b=8.0
+print(a,b,type(a),type(b))
+
+
+# In[45]:
+
+
+a=4
+b=2.0
+c=a*b
+print(c,type(c))
+
+
+# In[46]:
+
+
+a=5
+print(a,type(a))
+b=2/a
+print(b,type(b))
+
+
+# In[51]:
+
+
+x=20
+y=bool(a)
+print("The boolean of x is:",y)
+
+
+# In[49]:
+
+
+x=0
+y=bool(x)
+print("The boolean of x is:",y)
+
+
+# In[ ]:
+
+
+
+
